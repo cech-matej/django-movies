2d66c5b08c08615eee53e45e795e4a2ab522a061
cech-matej <cech-matej@seznam.cz>
2021-04-29 17:16:55 +0200

    Přizpůsobení administračního rozhraní

--- filmweb/settings.py
+++ filmweb/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies',
     'bootstrap_pagination',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'movies.context_processors.genres',
             ],
         },
     },
